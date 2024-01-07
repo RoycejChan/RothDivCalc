a04aa849706d6eac616c9c841fba2a7210240980
Roycej-Chan <Rouyce23@gmail.com>
2024-01-06 18:01:00 -0800

    sdfsf

--- calcBackend/calcBackend/calcBackend/settings.py
+++ calcBackend/calcBackend/calcBackend/settings.py
@@ -15,17 +15,10 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
-DEBUG = 'RENDER' not in os.environ
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
-
-ALLOWED_HOSTS = ["https://rothdividendcalc.onrender.com/", "https://rothdividendcalc.onrender.com/divCalc"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
