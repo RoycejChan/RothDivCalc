ae91166ccc042fec527827db608dfe611f60571a
Roycej-Chan <Rouyce23@gmail.com>
2024-01-06 18:25:10 -0800

    asda

--- calcBackend/calcBackend/calcBackend/settings.py
+++ calcBackend/calcBackend/calcBackend/settings.py
@@ -15,6 +15,10 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
